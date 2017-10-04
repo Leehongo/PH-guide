16faaba876faa2bf6c7a61c04e19ab3e2ea3be80
john lee mangahas <johnleemangahas@yahoo.com>
2017-10-04 15:11:43 +0800

    update glide, remove image scale in xml

--- app/src/main/java/com/ufc/phdestination/ph_guide/Controller/tools/Utilities.java
+++ app/src/main/java/com/ufc/phdestination/ph_guide/Controller/tools/Utilities.java
@@ -2,16 +2,20 @@ package com.ufc.phdestination.ph_guide.Controller.tools;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.Target;
 import com.ufc.phdestination.ph_guide.R;
 
@@ -21,39 +25,47 @@ import com.ufc.phdestination.ph_guide.R;
 
 public class Utilities {
 
+    static RequestOptions myOptions = new RequestOptions()
+            .placeholder(R.drawable.placeholder)
+            .error(R.drawable.placeholder)
+            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+            .centerCrop()
+            .skipMemoryCache(true);
+
     public static void loadImageFromURL(@NonNull Context context, @NonNull final ProgressBar progressBar, ImageView imageView, String url){
         Glide.with(context)
                 .load(url)
-                .placeholder(R.drawable.placeholder)
-                .error(R.drawable.placeholder)
-                .crossFade()
-                .listener(new RequestListener<String, GlideDrawable>() {
+                .listener(new RequestListener<Drawable>() {
                     @Override
-                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+
                         progressBar.setVisibility(View.GONE);
                         return false;
                     }
 
                     @Override
-                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
                         progressBar.setVisibility(View.GONE);
                         return false;
                     }
                 })
-                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .apply(myOptions)
                 .into(imageView);
     }
 
     public static void loadImageFullSizeFromURL(Context context, ImageView imageView, String url){
         Glide.with(context)
                 .load(url)
-                .placeholder(R.drawable.placeholder)
-                .error(R.drawable.placeholder)
-                .crossFade()
-                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .apply(myOptions.circleCrop())
                 .into(imageView);
     }
-    //TODO use  .placeholder(R.drawable.loading_spinner)
+
+    public static void loadAsRoundImageFromURL(Context context, ImageView imageView, String url){
+        Glide.with(context)
+                .load(url)
+                .apply(myOptions)
+                .into(imageView);
+    }
 
     public static void getTransparentStatusBar(Activity myActivityReference) {
         myActivityReference.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
